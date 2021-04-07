9829700932739b141841a7ef019ded1a74b9511e
danailkostov <dankostov359@gmail.com>
2021-04-07 08:44:10 +0300

    clean the charts code a little bit

--- src/components/Popular/index.js
+++ src/components/Popular/index.js
@@ -1,59 +1,11 @@
-// STEP 1 - Include Dependencies
-// Include react
 import React from "react";
-
-// Include the react-fusioncharts component
 import ReactFC from "react-fusioncharts";
-
-// Include the fusioncharts library
 import FusionCharts from "fusioncharts";
-
-// Include the chart type
 import Column2D from "fusioncharts/fusioncharts.charts";
-
-// Include the theme as fusion
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.zune";
-
-// Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
 
-// STEP 2 - Chart Data
-const chartData = [
-  {
-    label: "Venezuela",
-    value: "290",
-  },
-  {
-    label: "Saudi",
-    value: "260",
-  },
-  {
-    label: "Canada",
-    value: "180",
-  },
-  {
-    label: "Iran",
-    value: "140",
-  },
-  {
-    label: "Russia",
-    value: "115",
-  },
-  {
-    label: "UAE",
-    value: "100",
-  },
-  {
-    label: "US",
-    value: "30",
-  },
-  {
-    label: "China",
-    value: "30",
-  },
-];
-
-const Popular = ({data}) => {
+const Popular = ({ data }) => {
   const chartConfigs = {
     type: "column2d", // The chart type
     width: "100%", // Width of the chart
@@ -66,10 +18,7 @@ const Popular = ({data}) => {
         yAxisName: "stars",
         theme: "zune",
         palettecolors: "2CAEBA,5D62B5,FFC533,F2726F,8D6E63",
-        // showValues: "1",
         rotateValues: "0",
-        // placeValuesInside: "0",
-        // valueFontColor: "#5d62b5",
       },
       // Chart Data
       data: data,
--- src/components/Stars/index.js
+++ src/components/Stars/index.js
@@ -1,57 +1,9 @@
-// STEP 1 - Include Dependencies
-// Include react
 import React from "react";
-
-// Include the react-fusioncharts component
 import ReactFC from "react-fusioncharts";
-
-// Include the fusioncharts library
 import FusionCharts from "fusioncharts";
-
-// Include the chart type
 import Doughnut2D from "fusioncharts/fusioncharts.charts";
-
-// Include the theme as fusion
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.zune";
-
-// Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Doughnut2D, FusionTheme);
-
-// STEP 2 - Chart Data
-const chartData = [
-  {
-    label: "Venezuela",
-    value: "290",
-  },
-  {
-    label: "Saudi",
-    value: "260",
-  },
-  {
-    label: "Canada",
-    value: "180",
-  },
-  {
-    label: "Iran",
-    value: "140",
-  },
-  {
-    label: "Russia",
-    value: "115",
-  },
-  {
-    label: "UAE",
-    value: "100",
-  },
-  {
-    label: "US",
-    value: "30",
-  },
-  {
-    label: "China",
-    value: "30",
-  },
-];
 
 const Stars = ({ data }) => {
   const chartConfigs = {
